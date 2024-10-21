cc6ef0e49641ea56a34730dd6946e8caf12ec72a
vishnupatel45 <patelvishnu4518@gmail.com>
2024-10-21 18:19:00 +0530

    feat:contact us component made responsive

--- src/pages/subhomepages/contactus/contactus.jsx
+++ src/pages/subhomepages/contactus/contactus.jsx
@@ -8,27 +8,51 @@ import './contact.css'
 const ContactHomepage = () => {
     return (
         <>
+            <div>
+                {/* bi-geo-alt-fill */}
+            </div>
+            <div className="contactus">
+                <div>
+                    <div className="contactuscard">
+                        <div className="bi-envelope-fill contactIcon"></div>
+                        <div className=" ms-2 fw-medium">EMAIL <br /> email@gmail.com </div>
+                    </div>
+                    <div className="contactuscard">
+
+                        <div className="bi-phone-fill contactIcon"></div>
+                        <div className=" ms-2 fw-medium">PHONE <br />1230456789</div>
+                    </div>
+                    <div className="contactuscard">
+                        <div className="bi-geo-alt-fill contactIcon"></div>
+                        <div className="ms-2 fw-medium">
+                            Address
+                            <br />
+                            Hyderabad
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="contactHomepage">
-                <div className="bgcontactus border border-2">
+                <div className="bgcontactus">
                     <div className="Gettotuch">
-                        <div style={{ height: '90%', width: '50%' }}>
+                        <div className="subGettouch">
                             <div className=" fw-medium fs-3">
                                 Get in <span style={{ color: '#F88035' }}>Touch</span>
                             </div>
-                            <Formik 
-                                initialValues={{userName:'',userEmail:'',userNumber:'',select:''}}
+                            <Formik
+                                initialValues={{ userName: '', userEmail: '', userNumber: '', select: '' }}
                                 validationSchema={yup.object({
                                     userName: yup.string().required('Please enter your name'),
                                     userEmail: yup.string().email('Invalid email').required('Please enter your email'),
                                     userNumber: yup.number().required('Please enter your number'),
                                     select: yup.string().required('Please select an option'),
                                 })}
-                                onSubmit={async (userData)=>{
-                                    try{
-                                        await axios.post('http://localhost:7000/gettotuchuser',userData)
-                                        alert(userData.userName +' User Detials is updated successfull')
+                                onSubmit={async (userData) => {
+                                    try {
+                                        await axios.post('http://localhost:7000/gettotuchuser', userData)
+                                        alert(userData.userName + ' User Detials is updated successfull')
                                         console.log(userData);
-                                    }catch(error){
+                                    } catch (error) {
                                         console.log(error);
                                     }
                                 }}
@@ -36,39 +60,41 @@ const ContactHomepage = () => {
                                 {
                                     form => <Form className="mt-4">
                                         <div className="mb-3">
-                                            <Field name='userName' type='text' placeholder='Your Name' className="form-control w-75" />
+                                            <Field name='userName' type='text' placeholder='Your Name' className="form-control" />
                                             <ErrorMessage name="userName" />
                                         </div>
                                         <div className="mb-3">
-                                            <Field name='userEmail' type='text' placeholder='Your Email' className="form-control w-75" />
+                                            <Field name='userEmail' type='text' placeholder='Your Email' className="form-control" />
                                             <ErrorMessage name="userEmail" />
                                         </div>
                                         <div className="mb-3">
-                                            <Field name='userNumber' type='text' placeholder='Your Number' className="form-control w-75" />
+                                            <Field name='userNumber' type='text' placeholder='Your Number' className="form-control" />
                                             <ErrorMessage name="userNumber" />
                                         </div>
                                         <div>
-                                            <Field as="select" className="form-select w-75" name="select">
+                                            <Field as="select" className="form-select" name="select">
                                                 <option value="-1">Select</option>
                                                 <option value="socialmedia">Social Media</option>
                                                 <option value="NEW">New</option>
                                             </Field>
                                             <ErrorMessage name="select" component="div" />
                                         </div>
-                                        <button className="btn w-75 mt-4" type="submit" style={{backgroundColor:'#F88035'}}>SEND</button>
+                                        <button className="btn w-100 mt-4" type="submit" style={{ backgroundColor: '#F88035' }}>SEND</button>
 
                                     </Form>
                                 }
                             </Formik>
-                            <div className="d-flex align-items-center mt-5">
-                                <div className="bi-telephone-outbound fs-3 "></div>
-                                <div className=" ms-2 fw-medium">PHONE <br />1230456789</div>
-                                <div className="bi-envelope ms-3 fs-3"></div>
-                                <div className=" ms-2 fw-medium">EMAIL <br /> email@gmail.com </div>
+                            <div className="contexts">
+                                <div className="d-flex align-items-center mt-5">
+                                    <div className="bi-telephone-outbound fs-3 "></div>
+                                    <div className=" ms-2 fw-medium">PHONE <br />1230456789</div>
+                                    <div className="bi-envelope ms-3 fs-3"></div>
+                                    <div className=" ms-2 fw-medium">EMAIL <br /> email@gmail.com </div>
+                                </div>
                             </div>
 
                         </div>
-                        <div style={{ height: '90%', width: '50%', backgroundColor:'gray' }}>
+                        <div className="subGettouch bg-dark-subtle">
 
                         </div>
                     </div>
